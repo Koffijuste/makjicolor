5dbc2f42efa92764e8f6bb0896cb4621ca3f3a7c
Morning Star <koffioijuste@icloud.com>
2025-12-14 13:05:49 +0100

    requirement et init modifier

--- __init__.py
+++ __init__.py
@@ -25,18 +25,14 @@ def create_app():
     database_url = os.environ.get('DATABASE_URL')
 
     if database_url:
-        if database_url.startswith("postgres://"):
-            database_url = database_url.replace(
-                "postgres://",
-                "postgresql+psycopg://",
-                1
-        )
-        elif database_url.startswith("postgresql://"):
-            database_url = database_url.replace(
-                "postgresql://",
-                "postgresql+psycopg://",
-                1
-        )
+        database_url = database_url.replace("postgres://", "postgresql+psycopg2://", 1)
+        database_url = database_url.replace("postgresql://", "postgresql+psycopg2://", 1)
+        database_url = database_url.replace("postgresql+psycopg://", "postgresql+psycopg2://", 1)
+        database_url = database_url.replace("mysql://", "mysql+pymysql://", 1)
+    # Pour SQLite, s'assurer d'avoir un fichier
+        if database_url.startswith("sqlite://"):
+            if database_url == "sqlite://":
+                database_url = "sqlite:///../instance/makjicolor.db"
 
         app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     else:
